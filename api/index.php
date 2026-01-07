a9fcffc70d02c16286bee8401445ce970773eaf2
natsuki70 <sendisetiandi050500@gmail.com>
2026-01-07 20:51:36 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,16 @@
 <?php
 
-// Mengarahkan Vercel untuk membaca file index Laravel yang asli
+// Membuat folder storage sementara di Vercel
+$storageFolders = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/cache',
+];
+
+foreach ($storageFolders as $folder) {
+    if (!is_dir($folder)) {
+        mkdir($folder, 0777, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
