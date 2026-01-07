c61d177d7ad17a7ac617f64582199a1698635450
natsuki70 <sendisetiandi050500@gmail.com>
2026-01-07 21:22:05 +0700

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+<?php
+$storageFolders = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/cache',
+];
+foreach ($storageFolders as $folder) {
+    if (!is_dir($folder)) {
+        mkdir($folder, 0777, true);
+    }
+}
+=======
+<?php
+$storageFolders = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/cache',
+];
+foreach ($storageFolders as $folder) {
+    if (!is_dir($folder)) {
+        mkdir($folder, 0777, true);
+    }
+}
+>>>>>>> 669716d01e9021675be4b3b148926841d8d324c3
+require __DIR__ . '/../public/index.php';
